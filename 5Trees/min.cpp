5239044734ab21aa0b58e4a2234705dde56d15f8
HarshRepal <harsh.repal2003@gamil.com>
2023-02-11 11:25:51 +0530

    Trees D3

--- 5Trees/min.cpp
+++ 5Trees/min.cpp
@@ -101,6 +101,51 @@ class Tree{
 
 
     }
+
+
+
+
+void IPreorder(Node *p)
+{
+ struct Stack stk;
+ Stackcreate(&stk,100);
+ 
+ while(p || !isEmptyStack(stk))
+ {
+ if(p)
+ {
+ printf("%d ",p->data);
+ push(&stk,p);
+ p=p->lchild;
+ }
+ else
+ {
+ p=pop(&stk);
+ p=p->rchild;
+ }
+ }
+}
+void IInorder(struct Node *p)
+{
+ struct Stack stk;
+ Stackcreate(&stk,100);
+ 
+ while(p || !isEmptyStack(stk))
+ {
+ if(p)
+ {
+ push(&stk,p);
+ p=p->lchild;
+ }
+ else
+ {
+ p=pop(&stk);
+ printf("%d ",p->data);
+ p=p->rchild;
+ }
+ }
+}
+
 };
 
 
